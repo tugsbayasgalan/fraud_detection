cb0f50ae45f3c33e9fc04f32d6bb196ef3f2e309
tugsuu <tugsuu@mit.edu>
2018-12-27 14:47:27 -0500

    Added comments, implemented cache, and updated README.md

--- fraud_ip_detection.py
+++ fraud_ip_detection.py
@@ -21,31 +21,51 @@ def compute_distance(ip_address_1, ip_address_2):
 
     #look at https://en.wikipedia.org/wiki/Haversine_formula for the formula
     a = math.sin(lat_difference/2)**2 + math.cos(lat1)*math.cos(lat2)*(math.sin(long_difference/2)**2)
-    c = 2*math.asin(math.sqrt(a))
-    distance = R*c
+    b = 2*math.asin(math.sqrt(a))
+    distance = R*b
     #this is to output with 1 significant figure after decimal point
     return round(distance, 1)
 
 
+class AddressInfo:
+
+    def __init__(self, classification, location_info):
+        self.classification = classification
+        self.location_info = location_info
+    def get_classification(self):
+        return self.classification
+    def get_location_tuple(self):
+        return self.location_info
+
 class FraudIpDetection:
 
-    def __init__(self, filePath, acc_token='acaea93a2d4b64'):
+    def __init__(self, filePath, acc_token):
 
         self.filePath = filePath
         self.ip_handler = ipinfo.getHandler(acc_token)
+        #stores AddressInfo objects as values and key as their ip addresses
         self.data_map = {}
+        #cache the result from ipinfo for faster lookup
+        self.cache = {}
+        #build the database before scoring ip addresses
         self.parse_data()
 
     def parse_data(self):
         with open(self.filePath) as f:
             for line in f:
                 classification, ip_address = line.split()
-                loc_info = self.get_loc_info(ip_address)
-                self.data_map[ip_address] = {'classification': classification, 'loc': loc_info}
+                if ip_address in self.cache:
+                    loc_info = self.cache[ip_address]
+                else:
+                    loc_info = self.get_loc_info(ip_address)
+                self.data_map[ip_address] = AddressInfo(classification, loc_info)
 
     def get_loc_info(self, ip_address):
+
         try:
+            #gets metadata about the ip address
             details = self.ip_handler.getDetails(ip_address)
+            #gets the physical location as latitude and longitude
             loc_info = details.loc
             latitude, longitude = loc_info.split(",")
             return (float(latitude), float(longitude))
@@ -53,16 +73,16 @@ class FraudIpDetection:
             raise Exception("Wrong input")
 
     def score(self, point):
-
         point_tuple = self.get_loc_info(point)
         distance, classification = self.find_closest(point_tuple)
+        #if the closest point is FRAUD, increase the score by a factor of two
         if classification == FRAUD:
             return distance*2
         return distance
 
     def find_closest(self, point_tuple):
-        distance_array = [(compute_distance(point_tuple, self.data_map[i]['loc']),
-                          self.data_map[i]['classification']) for i in self.data_map]
+        distance_array = [(compute_distance(point_tuple, self.data_map[i].get_location_tuple()),
+                          self.data_map[i].get_classification()) for i in self.data_map]
         return min(distance_array, key=lambda x: x[0])
 
 
@@ -77,7 +97,7 @@ def main(arguments):
             except:
                 print "Something is wrong with the input. Please provide a valid IP address"
     except KeyboardInterrupt:
-        print "Program is stopped by a user"
+        print "\nProgram is stopped by a user"
         sys.exit()
 
 if __name__ == '__main__':
